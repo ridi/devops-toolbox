e8fe5e00a1a8124a7f19dca603811cb6131b066c
Kang Ki Tae <kt.kang@ridi.com>
2018-06-28 11:13:51 +0900

    Replace /etc/munin string with $munin_dir variable

--- munin/uptime_warning.sh
+++ munin/uptime_warning.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# uptime_warning.sh [label] [group] [munin_node] [uptime_now] [uptime_max]
+
 label=$1
 if [ "$label" != "uptime" ]
 then
@@ -25,7 +27,7 @@ group_arr=(${group//-/ })
 munin_node_config=$(IFS=";" ; echo "${group_arr[*]};$munin_node")
 
 # Reset munin.config
-lines=`cat -n /etc/munin/munin.conf | sed -n "/$munin_node_config\]/,/\[/p" | awk '{print $1}' | sed -e 2b -e '$!d'`
+lines=`cat -n $munin_dir/munin.conf | sed -n "/$munin_node_config\]/,/\[/p" | awk '{print $1}' | sed -e 2b -e '$!d'`
 lines=(${lines//' '/ })
 sed "${lines[0]},${lines[1]} s/uptime.uptime.warning $uptime_max/uptime.uptime.warning $uptime_new_max/" $munin_dir/munin.conf > $munin_dir/munin_tmp.conf
 mv $munin_dir/munin.conf $munin_dir/munin.conf.$(date +%s) && mv $munin_dir/munin_tmp.conf $munin_dir/munin.conf
