17603a2e43e2dedb25fd1f2d8a9cc50a46e6041e
Jihun Kim <devgrapher@gmail.com>
2019-07-29 11:56:35 +0900

    Log munin_asana script params

--- munin/run_uptime_warning.sh
+++ munin/run_uptime_warning.sh
@@ -6,6 +6,9 @@ source $base_dir/.env
 # Run script with given prameters
 $base_dir/uptime_warning.sh $@
 
+# Log params for debuggin
+echo $@ >> /var/log/munin/munin_asana.log
+
 # Check the last return code
 if [ $? != 0 ]; then
   mail -s "Uptime warning script :: $(hostname)" ${EMAIL_ON_ERROR} <<< "Script Error."
